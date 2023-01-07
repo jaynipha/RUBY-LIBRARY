318e4a45161f7226c97e724d134b00da68d72db0
Prince Leo <princeleo400.ohagamichael@gmail.com>
2023-01-07 14:32:34 +0100

    first commit

---
+++ config/dbConn.js
@@ -0,0 +1,24 @@
+const mongoose = require('mongoose');
+
+const connectDB = async () => {
+    try {
+        await mongoose.connect(process.env.DATABASE_URI, {
+            useUnifiedTopology: true,
+            useNewUrlParser: true
+        });
+    } catch (err) {
+        console.error(err);
+    }
+}
+
+const database = mongoose.connection;
+
+database.on("error", (error) => {
+  console.log(error);
+});
+
+database.once("connected", () => {
+  console.log("Database Connected");
+});
+
+module.exports = connectDB
